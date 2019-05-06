34375d50c62dd9f699c837af8b6076e0b7a643a6
Aries <aries120901@gmail.com>
2019-05-06 22:51:41 +0800

    標題換中文

--- WebApplication1/Models/tBookData.cs
+++ WebApplication1/Models/tBookData.cs
@@ -11,21 +11,34 @@ namespace WebApplication1.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel;
+
     public partial class tBookData
     {
         public int BOOK_ID { get; set; }
+        [DisplayName("書籍名稱")]
         public string BOOK_NAME { get; set; }
+        [DisplayName("類別")]
         public string BOOK_CLASS_ID { get; set; }
+        [DisplayName("書籍作者")]
         public string BOOK_AUTHOR { get; set; }
+        [DisplayName("書籍購書日期")]
         public Nullable<System.DateTime> BOOK_BOUGHT_DATE { get; set; }
+        [DisplayName("出版商")]
         public string BOOK_PUBLISHER { get; set; }
+        [DisplayName("內容簡介")]
         public string BOOK_NOTE { get; set; }
+        [DisplayName("狀態")]
         public string BOOK_STATUS { get; set; }
+        [DisplayName("書籍保管人")]
         public string BOOK_KEEPER { get; set; }
+        [DisplayName("建立時間")]
         public Nullable<System.DateTime> CREATE_DATE { get; set; }
+        [DisplayName("建立使用者")]
         public string CREATE_USER { get; set; }
+        [DisplayName("修改時間")]
         public Nullable<System.DateTime> MODIFY_DATE { get; set; }
+        [DisplayName("修改使用者")]
         public string MODIFY_USER { get; set; }
     }
 }
